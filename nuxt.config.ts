e511850a077c26244c72f33efac9ce07568f434b
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-05-30 18:18:26 +0900

    fix: change base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -161,7 +161,7 @@ export default {
   // 404 ページの設定
   router: {
     // TODO: any 型やめる
-    base: '/github-pages/',
+    base: '/',
     extendRoutes(routes: any, resolve: any) {
       routes.push({
         name: 'custom',
